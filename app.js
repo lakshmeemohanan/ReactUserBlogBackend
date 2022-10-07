ab26b340cafa80cd79527939540ba2eafa1b7713
lakshmee <lakshmee.mohan12@gmail.com>
2022-10-07 05:03:17 -0700

    added new react app front end version

--- app.js
+++ app.js
@@ -12,20 +12,25 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public')));
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     next();
-});
+});*/
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+/*app.use((req, res, next) => {
     const error = new HttpError('Could not find this route', 404);
     return next(error);
-});
+});*/
 
 app.use((error, req, res, next) => {
     if (req.file) {
